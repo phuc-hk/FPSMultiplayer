b932461993e0f524c1d6880e3fa3c70ac6e7a7c8
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-03-31 13:25:00 +0700

    Add anonymous sign in

---
+++ Assets/FPS/Scripts/BackendServices/Authentication/AnonymousAuth.cs
@@ -0,0 +1,56 @@
+using Firebase.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AnonymousAuth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject loginMessage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuestButton()
+    {
+        SignInAnonymously();
+    }
+
+   void SignInAnonymously()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})", result.User.DisplayName, result.User.UserId);
+            QuestLoginSuccess();
+        });
+    }
+
+    public void QuestLoginSuccess()
+    {
+        loginMessage.gameObject.SetActive(true);
+        SceneManager.LoadScene(3);
+    }
+}
